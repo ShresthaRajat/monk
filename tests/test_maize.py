f89b108063d4243449cad4c3b3974e437ac7452a
Rajat Shrestha W <rajat.shrestha@eydean.com>
2020-03-19 09:18:25 +0545

    Changed the tests for the new refactored maze.py

--- tests/test_maize.py
+++ tests/test_maize.py
@@ -1,43 +1,46 @@
-import src.Maze as m
-import pytest  # !NOUIC
-import random
+import src.maze as maze
+import pytest  # noqa
 
 
 def test_draw_random_check_maize_part():
-    output = m.draw_random(10)
-    assert type(output[0]) == type([])
+    sample = maze.Maze()
+    output = sample.draw_random()
+    assert isinstance(output[0], list)
 
 
 def test_draw_random_check_maize_solution():
-    output = m.draw_random(10)
-    assert type(output[4]) == type([])
+    sample = maze.Maze()
+    output = sample.draw_random()
+    assert isinstance(output[4], list)
 
 
 def test_draw_random_maze_validity_check():
-    output = m.draw_random(10)
-    maze = output[0]
-    assert len(maze) > 1
+    sample = maze.Maze()
+    output = sample.draw_random()
+    points = output[0]
+    assert len(points) > 1
 
 
 def test_draw_random_solution_validity_check():
-    output = m.draw_random(10)
+    sample = maze.Maze()
+    output = sample.draw_random()
     solution = output[4]
     assert len(solution) > 1
 
 
-def test_genc_coordinates_is_number_pairs():
-    output = m.genc(10)
-    random_coordinates = random.choice(output)
-    assert len(random_coordinates) == 2
+# def test_genc_coordinates_is_number_pairs():
+#     output = m.genc(10)
+#     random_coordinates = random.choice(output)
+#     assert len(random_coordinates) == 2
 
 
-def test_genc_coordinates_is_number_pairs_int():
-    output = m.genc(10)
-    random_coordinates = random.choice(output)
-    assert (type(random_coordinates[0]) == type(1))
+# def test_genc_coordinates_is_number_pairs_int():
+#     output = m.genc(10)
+#     random_coordinates = random.choice(output)
+#     assert isinstance(type(random_coordinates[0], int)
 
 
-def test_genc_coordinates_is_number_pairs_int_second_digit():
-    output = m.genc(10)
-    random_coordinates = random.choice(output)
-    assert (type(random_coordinates[1]) == type(1))
+# def test_genc_coordinates_is_number_pairs_int_second_digit():
+#     output=m.genc(10)
+#     random_coordinates=random.choice(output)
+#     assert isinstance(type(random_coordinates[1]), int)
