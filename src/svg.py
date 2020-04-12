f0a070d91c1ab038e236c0a3292ce6272d46cf5a
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-04-12 23:34:34 +0545

    Edit svg generetor

--- src/svg.py
+++ src/svg.py
@@ -1,27 +1,29 @@
 import svgwrite
 import maze_generator as mz
 
+class
 
-def svg(x, y, stroke, stroke_width, stroke_opacity, dwg):
-    dwg.add(dwg.line(
-        x, 
-        y, 
-        stroke=stroke, 
-        stroke_width=stroke_width, 
+
+def svg(x, y, stroke, stroke_width, stroke_opacity, svg_drawing):
+    svg_drawing.add(svg_drawing.line(
+        x,
+        y,
+        stroke=stroke,
+        stroke_width=stroke_width,
         stroke_opacity=stroke_opacity,
         stroke_linecap="square"
-        ))
+    ))
 
 
 def create_svg(maze_points, maze_last_point, maze_solution, seed, t=False):
-    # dwg = svgwrite.Drawing('data/'+seed+'.svg', profile='full')
-    dwg = svgwrite.Drawing('src/static/test.svg', profile='tiny')
+    svg_drawing = svgwrite.Drawing('src/static/data/'+seed+'.svg', profile='full')
+    # svg_drawing = svgwrite.Drawing('src/static/test.svg', profile='tiny')
     for i in range(len(maze_points)-1):
         ax = maze_points[i][0] + maze_last_point + 2
         ay = maze_points[i][1] + maze_last_point + 2
         bx = maze_points[i+1][0] + maze_last_point + 2
         by = maze_points[i+1][1] + maze_last_point + 2
-        svg([ax, ay], [bx, by], svgwrite.rgb(5, 5, 5, '%'), 3, 0.99, dwg)
+        svg([ax, ay], [bx, by], svgwrite.rgb(5, 5, 5, '%'), 3, 0.99, svg_drawing)
     if t:
         for j in range(len(maze_solution)-1):
             cx = maze_solution[j][0] + maze_last_point + 1
@@ -30,14 +32,15 @@ def create_svg(maze_points, maze_last_point, maze_solution, seed, t=False):
             dy = maze_solution[j+1][1] + maze_last_point + 1
             # if cx == dx:
             #     cy = cy + 1
-            #     dy = dy 
+            #     dy = dy
             # else :
-            #     cy = cy 
+            #     cy = cy
             #     dy = dy + 1
-            svg([cx, cy], [dx, dy], svgwrite.rgb(10, 30, 10, '%'), 1, 0.7, dwg)
+            svg([cx, cy], [dx, dy], svgwrite.rgb(10, 30, 10, '%'), 1, 0.7, svg_drawing)
         padding = [(maze_last_point*2)+3, (maze_last_point*2)+3]
-        svg([0,0], padding, svgwrite.rgb(5, 5, 5, '%'), 1, 0.0, dwg)
-    dwg.save()
+        svg([0, 0], padding, svgwrite.rgb(5, 5, 5, '%'), 1, 0.0, svg_drawing)
+        print(svg_drawing)
+    svg_drawing.save()
 
 
 maze = mz.Maze(3, 4)
