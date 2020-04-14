8fadb093f8708107091c15115b50ac05ecabb538
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-04-14 11:43:40 +0545

    Create schema.py for all of the schema

---
+++ src/schema.py
@@ -0,0 +1,43 @@
+import maze_generator as mz
+import graphene
+from graphene import Int, Float
+
+
+class MazeType(graphene.ObjectType):
+    """A Simple mazetype object"""
+    points = graphene.List(graphene.List(Int), description="The list of the co-ordinates of the lines to be generated")
+    size = graphene.Int(description="The size of the maze")
+    seed = graphene.String(description="The seed of the maze")
+    color = graphene.List(graphene.List(Float), description="The list of colours of the layers Comming Soon!")
+    solution = graphene.List(graphene.List(Int), description="The list of points of the solution")
+    layers = graphene.Int(description="The numbers of layer of the maze")
+
+
+class MazeQuery(graphene.ObjectType):
+    """Various queries related to generating or retreving mazes"""
+    get_maze = graphene.Field(
+        MazeType,
+        size=graphene.Int(description="The size of the maze to be to generated"),
+        color=graphene.Boolean(description="The parameter to color the layers of the maze (comming Soon!)"),
+        layer_approx=graphene.Int(description="The approximate numbers of layers for the maze to be generated"),
+        seed=graphene.String(description="The Seed to generate the maze"),
+        description="Generates the maze according to the size or seed"
+    )
+
+    def resolve_get_maze(root, info, layer_approx=1, color=False, size=1, seed=""):
+        if seed != "":
+            maze = mz.Maze(0, 0, seed)
+        else:
+            maze = mz.Maze(layer_approx, size)
+        maze_package = {
+            "points": maze.points,
+            "solution": maze.solution,
+            "layers": maze.layers,
+            "size": maze.size,
+            "seed": maze.seed,
+            "color": maze.color
+        }
+        return maze_package
+
+
+MazeSchema = graphene.Schema(query=MazeQuery)
