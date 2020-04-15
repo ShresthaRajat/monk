a8be55d5c388d8e584da48abbc875ce23e478c57
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-04-15 11:46:09 +0545

    Rename Procfile and add app

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from src.schema import MazeSchema
+
+
 from flask import Flask
+from flask_cors import CORS
+from flask_graphql import GraphQLView
 
 app = Flask(__name__)
+CORS(app)
+
+schema = MazeSchema
 
 
 @app.route("/")
@@ -8,5 +16,13 @@ def index():
     return "<h1>Endpoints on api/</h1>"
 
 
+app.add_url_rule(
+    "/api", view_func=GraphQLView.as_view(
+        "graphql",
+        schema=schema,
+        graphiql=True
+    )
+)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8808)
